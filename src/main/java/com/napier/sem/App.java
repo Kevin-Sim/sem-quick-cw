dcda214e56a863bf853c9885370b0dd6a1fb38b9
Kevin Sim <k.sim@napier.ac.uk>
2026-09-23 08:19:40 +0100

    updated pom

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -32,7 +32,7 @@ public class App
 			try
 			{
 				// Wait a bit for db to start
-				Thread.sleep(1000);
+				Thread.sleep(10000);
 				// Connect to database
 				con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false&allowPublicKeyRetrieval=true", "root", "example");
 				System.out.println("Successfully connected");
